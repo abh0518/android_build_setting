e813ba6042b9a8a35e53c3004938184ad963fb55
AhnByungHyun <abh0518@gmail.com>
2016-06-08 16:07:13 +0900

    * 기본 형태 잡음

---
+++ app_library/src/main/java/net/abh0518/applibrary/Version.java
@@ -0,0 +1,9 @@
+package net.abh0518.applibrary;
+
+/**
+ * Created by 1001241 on 2016. 6. 8..
+ */
+public class Version {
+    public final static int versionCode = 1;
+    public final static String versionName = "1.0.0";
+}
---
+++ make_release_package.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+package_app_dir="release_app_package"
+package_module_dir="release_module_package"
+
+# build.gradle 파일에서 버전 정보를 가져온다.
+appVersionCode=$(echo "$fullName"  | sed -n '/versionCode /p' ./app/build.gradle)
+appVersionName=$(echo "$fullName"  | sed -n '/versionName /p' ./app/build.gradle)
+[[ $appVersionCode =~ [0-9]+ ]]
+appVersionCode=${BASH_REMATCH[0]}
+[[ $appVersionName =~ [0-9]+.[0-9+.[0-9]+ ]]
+appVersionName=${BASH_REMATCH[0]}
+
+# 패키지 디렉토리 및 그래들 빌드 환경을 초기화
+rm -rf $package_app_dir
+mkdir $package_app_dir
+rm -rf $package_module_dir
+mkdir $package_module_dir
+./gradlew clean
+
+# 스테이징 디버그, 상용 디버그, 배포용 빌드
+./gradlew assembledevDebug
+find ./app -name '*-debug.apk' -exec mv {} ./$package_app_dir/ \;
+
+./gradlew assemblestagingDebug
+find ./app -name '*-debug.apk' -exec mv {} ./$package_app_dir/ \;
+
+./gradlew assembleproductDebug
+find ./app -name '*-debug.apk' -exec mv {} ./$package_app_dir/ \;
+
+./gradlew assembleproductRelease
+find ./app -name '*-unsigned.apk' -exec mv {} ./$package_app_dir/ \;
+find ./app_library -name '*.aar' -exec mv {} ./$package_module_dir/ \;
+
